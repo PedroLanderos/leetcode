8df7b88134a1b7579cf5b6bbb8695c10c40c2d62
PedroLanderos <developerpjlc@gmail.com>
2025-02-01 22:26:03 -0600

    two sum

---
+++ twosum/twosum/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> arrayNumbers = new Dictionary<int, int>();
+
+    
+    for(int i = 0; i < nums.Length; i++)
+    {
+        //revisar si el target - menos el elemento actual en el array existe en el map
+        if (arrayNumbers.ContainsKey(target - nums[i]))
+        {
+            //si no existe se agrega al map
+            return new int[] { arrayNumbers[(target - nums[i])], i };
+        }
+
+        arrayNumbers[nums[i]] = i;
+
+    }
+
+    return new int[] { -1, -1 };
+
+}
