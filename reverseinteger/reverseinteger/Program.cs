5d2b6e1768ecdb687cc7f91088250cbfd0469fdf
PedroLanderos <developerpjlc@gmail.com>
2025-02-02 09:23:40 -0600

    reverse Integer

---
+++ reverseinteger/reverseinteger/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics;
+using System.Reflection;
+
+int Reverse(int x)
+{
+
+    bool isNegative = x < 0;
+
+    long n = Math.Abs((long)x);
+
+    string reverseString = new string(n.ToString().Reverse().ToArray()).TrimEnd('0');
+
+    long reverse = reverseString == "" ? 0 : long.Parse(reverseString);
+
+    if (isNegative) reverse *= -1;
+
+    if (reverse < int.MinValue || reverse > int.MaxValue) return 0;
+
+    return (int)reverse;
+
+}
