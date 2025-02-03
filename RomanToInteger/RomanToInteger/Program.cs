587929249ae69dd4b4a0a5558c110bf20da9edf5
PedroLanderos <developerpjlc@gmail.com>
2025-02-03 09:45:34 -0600

    roman numbers

---
+++ RomanToInteger/RomanToInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿int RomanToInt(string s)
+{
+    Dictionary<char, int> romanNumbers = new Dictionary<char, int>() {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int total = 0;
+    int prevValue = 0;
+    //III
+    //IV
+    //recorrer de derecha a izquierda la cadena 
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        int currentValue = romanNumbers[s[i]]; // V -> 5
+
+        if (currentValue < prevValue)
+            total -= currentValue;
+        else total += currentValue;
+
+        prevValue = currentValue;
+    }
+
+    return total;
+}
--- palindromeNumber/palindromeNumber/Program.cs
+++ palindromeNumber/palindromeNumber/Program.cs
@@ -4,7 +4,7 @@ bool IsPalindrome(int x)
     if(x < 0) return false;
 
     string str = x.ToString();
-    string rev = new string(str.Reverse().ToString());
+    string rev = new string(str.Reverse().ToArray());
 
     return str == rev;
 
