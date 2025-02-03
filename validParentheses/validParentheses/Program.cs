6589634895e355778d6f3da8484ba22d8ff8af74
PedroLanderos <developerpjlc@gmail.com>
2025-02-02 19:19:55 -0600

    valid parentheses

---
+++ validParentheses/validParentheses/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Net;
+
+bool IsValid(string s)
+{
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> pairs = new Dictionary<char, char>()
+    {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
+
+    foreach (char c in s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            stack.Push(c);
+        }
+        else
+        {
+            if (stack.Count == 0 || stack.Peek() != pairs[c])
+            {
+                return false;
+            }
+            stack.Pop();
+        }
+    }
+
+    return stack.Count == 0;
+
+
+}
