64affc38bd72359285c29b0fd488f9ed8bbbb086
PedroLanderos <developerpjlc@gmail.com>
2025-02-04 10:08:11 -0600

    remove duplicates

---
+++ removeDuplicates/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace removeDuplicates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public int RemoveDuplicates(int[] nums)
+    {
+        //hash to save unique numbers
+        HashSet<int> n = new HashSet<int>();
+
+        int i = 0;
+
+        foreach(var x in nums)
+        {
+            if (n.Add(x)) //saving the numbers in the hash
+            {
+                //modifying the nums array by adding the unique element
+                nums[i] = x; 
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
