30ad7313b5783497853940714747f625d128fdeb
PedroLanderos <developerpjlc@gmail.com>
2025-02-04 09:52:45 -0600

    remove element

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RemoveElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        //[3,2,2,3
+        //3 <- val
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
